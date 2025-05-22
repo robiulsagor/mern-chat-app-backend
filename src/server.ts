4026c8afc232855de4b43143da0b4bad35803b27
robiulsagor <robiul100.me@gmail.com>
2025-05-22 19:33:42 +0600

    Backend Initiate

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGO_URI || ''
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  })
+  .catch((err) => console.error(err))
