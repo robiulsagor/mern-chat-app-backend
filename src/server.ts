99f5d35b3e30d7072ea76a97148f351160eac6b7
robiulsagor <robiul100.me@gmail.com>
2025-06-01 11:34:36 +0600

    feat: Socket io initialize

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import mongoose from 'mongoose'
+import http from 'http'
+import { Server } from 'socket.io'
 
 import authRoute from "./routes/auth.route"
 import userRoute from "./routes/user.route"
@@ -11,6 +13,18 @@ import msgRoute from "./routes/message.route"
 dotenv.config()
 
 const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }
+})
+
+
+
 app.use(cors(
   {
     origin: 'http://localhost:5173',
@@ -28,12 +42,21 @@ app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/message', msgRoute)
 
+
+
+io.on('connection', (socket) => {
+  console.log('New client connected- yes:', socket.id)
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id)
+  })
+})
+
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI || ''
 
 mongoose
   .connect(MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   })
   .catch((err) => console.error(err))
